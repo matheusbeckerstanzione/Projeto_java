43f3bc8bfc849f63a589f83f1e5e3d67e0b0bd02
marcobutrico <marcobutrico@gmail.com>
2025-08-12 21:36:45 -0300

    modifiquei o codigo no arquivo

--- funcaes/src/Main.java
+++ funcaes/src/Main.java
@@ -21,24 +21,24 @@ public class Main {
             return resultado;
 
     }
-
+//exercicio 1
     public static void mostrarMensagem(){
         System.out.println("Bem vindo");
 
     }
-
+//exercicio 2
     public static void  mostrarODobro (int num3) {//
         int resultado = num3 * 2 ;
         System.out.println(resultado);
 
     }
-
+//exercicio 3
     public static void  dividir (int num4, int num5) {
         int resultado = (num4 + num5) / 2 ;
         System.out.println(resultado);
 
     }
-
+//exercicio 4
     public static boolean par(int num6 ){
         boolean resultado = num6 % 2 == 0 ;
         System.out.println(resultado);
@@ -46,13 +46,32 @@ public class Main {
         return resultado;
 
     }
-
-    public static double calcularMedia (int num7, int num8){
-       double resultado = (num7 + num8) / 2;
+//exercicio 5
+    public static double calcularMedia (int num7, int num8){//criei a variavel
+       double resultado = (num7 + num8) / 2;//chamei a variavel para dividir e fazer a media
         System.out.println(resultado);
 
         return resultado;
 
+    }
+//Crie a função celsiusParaFahrenheit que receba uma temperatura em Celsius e exiba no console o valor convertido para Fahrenheit.
+//Fórmula: F = C * 1,8 + 32
+//Parâmetro: decimal (double ou float)
+//Retorno: nenhum (void)
+//Exemplo:
+//celsiusParaFahrenheit(0);   // Saída: 32.0°F
+//celsiusParaFahrenheit(25);  // Saída: 77.0°F
+
+    //exercicio extra
+    public static double celsiusParaFahrenheit (double temperatura){
+            double resultado = ((temperatura * 1.8) + 32);
+        return resultado;
+
+    }
+
+    public static double calcularAreaRetangulo(double altura, double largura){
+            double resultado = altura * largura;
+            return resultado;
     }
 
     public static void main(String[] args) {
@@ -72,21 +91,27 @@ public class Main {
 
         saudar("vini");//SO COLOCAR O NOME PQ A VARIAVEL JA VAI DIRETO
 
-        subtrair(8 , 4);
+        subtrair(8 , 4);//estou chamando a funcao
 
-        int somar = somar(10, 5);
+        int somar = somar(10, 5);//estou chamando a funcao
 
-        System.out.println(somar);
+        System.out.println(somar);//estou chamando a funcao
 
-        mostrarMensagem();
+        mostrarMensagem();//estou chamando a funcao
 
-        mostrarODobro(5);
+        mostrarODobro(5);//estou chamando a funcao
 
-        dividir(2 , 10);
+        dividir(2 , 10);//estou chamando a funcao
 
-        boolean resultado = par(4);
+        boolean resultado = par(4);//estou chamando a funcao
 
-        calcularMedia(10, 4);
+        calcularMedia(10, 4);//estou chamando a funcao
 
+        celsiusParaFahrenheit (30);
+        System.out.println(celsiusParaFahrenheit(30));
+
+        double areaRetangulo = calcularAreaRetangulo(30, 30);
+
+        System.out.println(areaRetangulo);
         }
     }
